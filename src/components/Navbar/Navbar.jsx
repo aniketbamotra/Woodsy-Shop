340fb6ae1f8b90dd3da889263d337f2aa35ed9d6
Aniket Bamotra <aniketbamotra@gmail.com>
2022-02-25 14:30:18 +0530

    menu animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,33 +19,16 @@ const Navbar = () => {
   return (
     <div>
       <div className="nav-container">
-        <div className="menu-container" onClick={toggleMenu}>
-          <svg
-            width="66"
-            height="18"
-            viewBox="0 0 66 18"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <line
-              x1="1.5"
-              y1="1.52362"
-              x2="63.57"
-              y2="1.52362"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
-            <line
-              x1="1.5"
-              y1="15.5236"
-              x2="63.57"
-              y2="15.5236"
-              strokeWidth="3"
-              strokeLinecap="round"
-            />
+        <div className={`menu-container menu ${isMenuActive && "opened"}`} onClick={toggleMenu} aria-label="Main Menu">
+          <svg width="100" height="100" viewBox="0 0 100 100">
+            <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+            <path class="line line2" d="M 20,50 H 80" />
+            <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
           </svg>
         </div>
+
         <div href="../Home/Home.jsx" className="logo-container"></div>
+
         <div className="cart-icon-container" onClick={toggleCart}>
           <svg
             width="35"
@@ -62,8 +45,7 @@ const Navbar = () => {
         </div>
       </div>
       <div
-        className={`slide-in-menu ${!isMenuActive && "slide-in-menu-close"}`
-        }
+        className={`slide-in-menu ${!isMenuActive && "slide-in-menu-close"}`}
       >
         <Link className="menu-link" to={'/'}>Home</Link>
         <Link className="menu-link" to={'/products'}>Shop</Link>
