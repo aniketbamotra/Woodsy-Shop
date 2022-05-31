ac8b1d3fdfe31509bd8647840ca048ff39b1edae
Aniket Bamotra <aniketbamotra@gmail.com>
2022-05-31 18:49:08 +0530

    changed cred

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,7 @@ else:
 #         'NAME': 'ebdb',
 #         'USER': 'woodsyDB',
 #         'PASSWORD': 'woodsyshoppe',
-#         'HOST': 'aa1c693z5rf07kl.cgwruacp2hxe.us-west-2.rds.amazonaws.com',
+#         'HOST': 'aa1kdt4wrn2a4rd.cgwruacp2hxe.us-west-2.rds.amazonaws.com',
 #         'PORT': '5432',
 #     }
 # }
@@ -154,8 +154,8 @@ USE_TZ = True
 # ]
 AWS_STORAGE_BUCKET_NAME = 'woodsy-static'
 AWS_S3_REGION_NAME = 'us-west-2'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = 'AKIAZFWWGSEZNCCAAJYO'
-AWS_SECRET_ACCESS_KEY = 'YQoMHRJjhN9dVq6a6Baisc4jrEOXfIkb5STrOxYL'
+AWS_ACCESS_KEY_ID = 'AKIAZFWWGSEZO7ERSE6L'
+AWS_SECRET_ACCESS_KEY = 'EpMDM2FsxWk3oDZ06/ohRbYx8cWmF1xr+3mNdyzm'
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
